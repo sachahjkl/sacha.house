87cad10f91a35956ecc6026f99a2ec7e681fdb03
Sacha FROMENT <sacha@hjkl.it>
2022-10-09 11:36:45 +0200

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +11,10 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true,
+			split: false
+		})
 	}
 };
 
