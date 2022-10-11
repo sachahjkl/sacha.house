95735f6761f5dc27650d1bd9877b006d3f0365d5
Sacha FROMENT <sacha@hjkl.it>
2022-10-11 21:39:28 +0200

    admin try

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -2,19 +2,25 @@ import { SECRET_PROXYCURL_BEARER_TOKEN } from '$env/static/private';
 import { PUBLIC_PROXYCURL_API_ENDPOINT } from '$env/static/public';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, getClientAddress }) => {
+	let credit_balance = 0;
 	console.log(`${PUBLIC_PROXYCURL_API_ENDPOINT}/proxycurl/api/credit-balance`);
-	const res = await fetch(`${PUBLIC_PROXYCURL_API_ENDPOINT}/proxycurl/api/credit-balance`, {
-		headers: {
-			Authorization: `Bearer ${SECRET_PROXYCURL_BEARER_TOKEN}`
-		},
-		mode: 'cors'
-	});
+	try {
+		const res = await fetch(`${PUBLIC_PROXYCURL_API_ENDPOINT}/proxycurl/api/credit-balance`, {
+			headers: {
+				Authorization: `Bearer ${SECRET_PROXYCURL_BEARER_TOKEN}`
+			},
+			mode: 'cors'
+		});
 
-	const { credit_balance } = (await res.json()) as { credit_balance: number };
+		({ credit_balance } = (await res.json()) as { credit_balance: number });
+	} catch (error) {
+		console.error("Echec de la récupération du crédit restant por l'api ProxyCURL");
+	}
 	// const clientAddress = getClientAddress();
 	// if (!auth(url.pathname, { clientAddress })) throw redirect(302, '/');
 	return {
-		credit_balance
+		credit_balance,
+		ip: getClientAddress()
 	};
 };
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import type { PageData } from "./$types";
 
+
+
+export let data : PageData;
 
 </script>
 
-Admin
+<pre><code>Admin - ip : "{data.ip}" - crédit : "{data.credit_balance}"</code></pre>
