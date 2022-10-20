a7286b5fa8792073860193003858f0859902216c
Sacha FROMENT <sacha@hjkl.it>
2022-10-20 16:39:36 +0200

    move prefetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	export let data: PageData;
 </script>
 
-<article class="prose" data-sveltekit-prefetch>
+<article class="prose">
 	<h1>
 		{SITE_TITLE}
 		<div class="visites hidden md:inline-block">
@@ -18,7 +18,7 @@
 	</h1>
 	<div class="divider" />
 	<h2 class="mt-2">Informations de contact / détails</h2>
-	<p>
+	<p data-sveltekit-prefetch>
 		➜ <a href="/a-propos">à propos de moi</a>
 	</p>
 	<p>
