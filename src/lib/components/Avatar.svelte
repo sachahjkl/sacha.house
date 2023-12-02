a8c8b50cb52dd688abe206d2d1f88d175162901e
Sacha FROMENT <sacha@sacha.house>
2023-12-02 13:32:14 +0100

    fix: margin on picture ?

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -18,7 +18,7 @@
 		hover:ring-secondary hover:scale-105 active:ring-success active:scale-95
 		transition-all ring-primary ring-offset-base-100 ring-offset-2;
 	}
-	.me {
+	.me, picture {
 		@apply m-0;
 	}
 </style>
